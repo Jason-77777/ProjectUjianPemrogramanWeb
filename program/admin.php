f5ce5c946f88fba68e30d937ccb6ca1f700a3156
Jason-77777 <jason.fan2828@gmail.com>
2024-03-15 18:29:44 +0700

    a
    a

--- program/admin.php
+++ program/admin.php
@@ -1,32 +1,41 @@
 <?php
+session_start(); // Mulai sesi
 
-//apakah ada data login yang dikirim
+// Periksa apakah data login telah disubmit
 if (isset($_POST["username"]) && isset($_POST["password"])) {
-    //jika data login benar, set session authenticated dan redirect ke secret
-    if ($_POST['username'] == 'user' && $_POST['password'] == 'password'){
+    // Periksa apakah nama pengguna dan kata sandi yang dimasukkan benar
+    if ($_POST['username'] == 'user' && $_POST['password'] == '123') {
+        // Jika benar, set sesi authenticated menjadi true
         $_SESSION['authenticated'] = true;
-        header("Location: secret.php");
-        die();
+        header("Location: db.php"); // Arahkan ke db.php
+        die(); // Hentikan eksekusi
     } else {
-        $error = 'Login salah';
+        header("Location: mainpage.php"); // Arahkan ke secret.php
+        die(); // Hentikan eksekusi
     }
 }
 ?>
-
-
+<!DOCTYPE html>
 <html>
-    <body>
-        <form method="post" action="db.php">
-            <div>
-                <label> Username </label>
-                <input type="text" name="username">
-            </div>
-            <div>
-                <label> Password </label>
-                <input type="password" name="password">
-            </div>
-            <div><input type="submit" value="login"></div>
-        </form>
-        <?php if (isset($error)) { echo "<p>$error<?P>";} ?>
-    </body>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="enter.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <form method="post" action="">
+                <div>
+                    <label> Username </label>
+                    <input type="text" name="username">
+                </div>
+                <div>
+                    <label> Password </label>
+                    <input type="password" name="password">
+                </div>
+                <div><input type="submit" value="login" class="login-button"></div>
+            </form>
+        </div>
+    </div>
+</body>
 </html>
