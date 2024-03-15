f5a5e5039340c60505ddcde9c60ba652153c1acd
Jason-77777 <jason.fan2828@gmail.com>
2024-03-15 18:04:13 +0700

    a
    a

--- program/admin.php
+++ program/admin.php
@@ -1,11 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+<?php
+
+//apakah ada data login yang dikirim
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+    //jika data login benar, set session authenticated dan redirect ke secret
+    if ($_POST['username'] == 'user' && $_POST['password'] == 'password'){
+        $_SESSION['authenticated'] = true;
+        header("Location: secret.php");
+        die();
+    } else {
+        $error = 'Login salah';
+    }
+}
+?>
+
+
+<html>
+    <body>
+        <form method="post" action="db.php">
+            <div>
+                <label> Username </label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <label> Password </label>
+                <input type="password" name="password">
+            </div>
+            <div><input type="submit" value="login"></div>
+        </form>
+        <?php if (isset($error)) { echo "<p>$error<?P>";} ?>
+    </body>
 </html>
